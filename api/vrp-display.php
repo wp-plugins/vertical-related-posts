cfce0a7c62f7744a4334170db01995050b3dd327
corneliucirlan <corneliucirlan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-22 18:31:21 +0000

    Version 1.2
    git-svn-id: https://plugins.svn.wordpress.org/vertical-related-posts/trunk@994888 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ api/vrp-display.php
@@ -0,0 +1,187 @@
+<?php
+
+	if (!class_exists("VerticalRelatedPosts")):
+		class VerticalRelatedPosts
+		{
+			/**
+			 * Plugin's settings
+			 */
+			private $cc_vrp_options;
+			private $values;
+
+			public function __construct()
+			{
+				global $post;
+				$this->cc_vrp_options = get_option('cc_vrp_options');
+				$this->values = get_post_custom($post->id);
+			}
+
+			/**
+			 * Get the tags for the current page
+			 */
+			private function getCurrentTags()
+			{
+				// Get all tags by ID from the current page 
+				$currentPostTags = get_the_tags();
+				$tags = array();
+				if ($currentPostTags)
+					foreach($currentPostTags as $tag)
+						$tags[] = $tag->term_id;
+
+				unset($currentPostTags);
+				return $tags;
+			}
+
+			/**
+			 * Get the number of posts to display
+			 */
+			private function getNumberToDisplay()
+			{
+				// Set the number of posts to be displayed 
+				$numberOfPosts = $this->cc_vrp_options['defaultNumberOfPosts'];
+				if (isset($this->values['numberOfDisplayedPosts'])):
+					$numberOfPosts = $this->values['numberOfDisplayedPosts'];
+					$numberOfPosts = (int)$numberOfPosts[0];
+				endif;
+				return $numberOfPosts;
+			}
+
+			/**
+			 * Query the WP DB for the posts
+			 */
+			private function getQuery($posts_per_page, $orderby, $post_type, $post__not_in, $post__in, $post_status)
+			{
+				$args = array(
+					'posts_per_page' => $posts_per_page,
+					'orderby' => $orderby,
+					'post_type' => $post_type,
+					'post__not_in' => $post__not_in,
+					'post__in' => $post__in,
+					'post_status' => $post_status
+					);
+				wp_reset_query();
+				return new WP_Query($args);
+			}
+
+			/**
+			 * Get the post types selected
+			 */
+			private function getSelectedPostTypes()
+			{
+				$postTypes = $this->cc_vrp_options['checkedPostTypes'];
+				if (isset($this->values['customPostTypesToUse'])):
+					if ($this->values['customPostTypesToUse'][0] == "on"):
+						unset($postTypes);
+						$availablePostTypes = get_post_types();
+						foreach ($availablePostTypes as $type)
+							if (strpos($this->values['checkedTypes'][0], $type)):
+								$postTypes[] = $type;
+							endif;
+					endif;
+				endif;
+				return $postTypes;
+			}
+
+			/**
+			 * Get all eligible posts for use
+			 */
+			private function getAllPosts($the_query, $tags)
+			{
+				$postsArray = array(); // the ids of posts that have 1 or more of current page's tags
+
+				while ($the_query->have_posts()):
+					$the_query->the_post();
+					$pageTags = get_the_tags();
+					if (is_array($pageTags))
+						foreach ($pageTags as $tag):
+							$t = $tag->term_id;
+							if (in_array($t, $tags))
+								if (!in_array($the_query->post->ID, $postsArray))
+									$postsArray[] = $the_query->post->ID;
+						endforeach;
+				endwhile;
+				return $postsArray;
+			}
+
+			/**
+			 * Single related post template
+			 */
+			private function displayArticle()
+			{
+				global $post;
+				$image = wp_get_attachment_image_src(get_post_thumbnail_id( $post->ID ), $this->cc_vrp_options['featuredImageSize']);
+				?>
+				<article>
+					<a href="<?php the_permalink() ?>" title="<?php the_title() ?>" rel="bookmark">
+						<?php if ($this->cc_vrp_options['displayTitle'] == 'on'): ?><h1 class='cc-vrp-article-title'><?php the_title() ?></h1><?php endif; ?>
+						<?php if ($this->cc_vrp_options['displayFeaturedImage'] == 'on'): ?><div><img src="<?php echo $image[0]; ?>"></div><?php endif; ?>
+					</a>
+					<?php if ($this->cc_vrp_options['displayExcerpt'] == 'on') the_excerpt(); ?>
+				</article>
+				<?php
+			}
+
+
+			/**
+			 *	DISPLAY VERTICAL RELATED POSTS 
+			 */
+			public function displayVerticalRelatedPosts()
+			{
+				// get all tags from current page
+				$tags = $this->getCurrentTags();
+
+				// get number of posts to be displayed
+				$numberOfPosts = $this->getNumberToDisplay();
+				
+				// get what post types to use
+				$postTypes = $this->getSelectedPostTypes();
+				
+				// Create the tags in common query
+				$the_query = $this->getQuery(-1, 'rand', $postTypes, array(get_the_ID()), null, 'publish');
+				
+				// get an array of all posts with common tags as current post
+				$postsArray = $this->getAllPosts($the_query, $tags);
+				
+				// Create the Related Posts Query
+				$the_query = $this->getQuery($numberOfPosts, 'rand', $postTypes, array(get_the_id()), $postsArray, 'publish');
+
+				// Main VRP block
+				$numberOfAvailablePosts = (have_posts()) ? sizeof($the_query->posts) : 0;
+
+				// display VRP on page only if allowed
+				$disableVRPOnPage = array_key_exists('disableVRPOnPage', $this->values) ? $this->values['disableVRPOnPage'][0] : "off";
+				if ($disableVRPOnPage == "off"):
+					?>
+					<div class="cc-vertical-related-posts">
+						<h1 class="cc-vrp-title"><?php echo $this->cc_vrp_options['relatedPostsTitle'] ?></h1>
+						<?php
+						// Display the Related Posts
+						if ($the_query->have_posts()): 
+							while ($the_query->have_posts()):
+								$the_query->the_post();
+								$this->displayArticle();
+							endwhile; 
+						endif;
+
+						// if there aren't enough posts, add random ones
+						if ($numberOfPosts > $numberOfAvailablePosts)
+							if ($this->cc_vrp_options['fillWithRandomPosts'] == 'on'):
+								$the_query = $this->getQuery($numberOfPosts-$numberOfAvailablePosts, 'rand', $this->cc_vrp_options['checkedPostTypes'], array(get_the_ID(), $postsArray), $postsArray, 'publish');
+								if ($the_query->have_posts()):
+									while ($the_query->have_posts()):
+										$the_query->the_post();
+										$this->displayArticle();
+									endwhile;
+								endif;
+							endif;
+					?>
+					</div>
+				<?php
+				endif;
+				// Reset the WP_Query
+				wp_reset_query();
+			}
+		}
+	endif;
+	
+?>
---
+++ api/vrp-metabox.php
@@ -0,0 +1,192 @@
+<?php
+	/**
+	 * 							ADD BUTTON TO SHOW/HIDE RELATED POSTS ON EVERY PAGE
+	 */
+
+	if (!class_exists("VerticalRelatedPostsMetabox")):
+		class VerticalRelatedPostsMetabox
+		{
+			/**
+			 * Plugin's settings
+			 */
+			public static $cc_vrp_options;
+
+			/**
+			 * Class constructor
+			 */
+			public function __construct($settings)
+			{
+				static::$cc_vrp_options = $settings;
+				/**
+				 * Hook into add_meta_boxes to register the plugin's metabox
+				 */
+				add_action('add_meta_boxes', array(__CLASS__, 'addRelatedMetaBox'));
+
+				/**
+				 * Hook into save_post to save metebox's data
+				 */
+				add_action('save_post', array($this, 'saveVRPMetabox'));
+
+				/**
+				 * Load custom CSS for options page
+				 */
+				add_action('admin_init', function() {
+					wp_register_style('admin-vrp', VRP_URI.'/css/admin-vrp.css', array(), VRP_VERSION);
+					wp_enqueue_style('admin-vrp');
+
+					wp_enqueue_script('vrp-metabox-jquery', VRP_URI.'/js/admin-vrp.js', array('jquery'), VRP_VERSION);
+				});
+			}
+
+			/**
+			 * Register the metabox
+			 */
+			public static function addRelatedMetaBox()
+			{
+				//var_dump(self::$cc_vrp_options);
+			    // get current post's type
+				if (in_array(get_post_type(get_the_ID()), self::$cc_vrp_options['checkedPostTypes'])):
+					$postType = get_post_type(get_the_ID());
+					add_meta_box('vertical-related-posts', 'Vertical Related Posts', array(__CLASS__, 'verticalRelatedPostsMetaBox'), $postType, 'normal', 'default' );  
+				endif;
+			}
+
+			/**
+			 * Create metabox
+			 */
+			static function verticalRelatedPostsMetaBox()  
+			{  
+			    // $post is already set, and contains an object: the WordPress post  
+			    global $post;
+			    $values = get_post_custom($post->id);
+			    
+			    $numberOfPosts = static::$cc_vrp_options['defaultNumberOfPosts'];
+			    $availablePostTypes = get_post_types();
+			    if (isset($values['numberOfDisplayedPosts'])):
+				    $numberOfPosts = $values['numberOfDisplayedPosts'];  
+			  		$numberOfPosts = (int)$numberOfPosts[0];
+			  	endif;
+
+			  	$checked = 'off';
+			  	if (isset($values['customPostTypesToUse'])) $checked = $values['customPostTypesToUse'][0];
+
+			  	$disabled = 'off';
+			  	if (isset($values['disableVRPOnPage'])) $disabled = $values['disableVRPOnPage'][0];
+			   
+			    // We'll use this nonce field later on when saving.  
+			    wp_nonce_field( 'my_meta_box_nonce', 'meta_box_nonce' ); 
+			    ?> 
+				
+				<div id="cc-vrp-wrapper">
+					<section class="cc-vrp-section">
+						<div class="cc-vrp-label">
+							Disable Vertical Related Posts for this page
+						</div>
+						<div class="cc-vrp-input">
+							<div class="onoffswitch" style="clear: right;">
+								<input style="display: none;" type="checkbox" name="disableVRPOnPage" class="onoffswitch-checkbox" id="disableVRPOnPage" <?php if ($disabled == "on") echo "checked" ?>>
+								<label class="onoffswitch-label" for="disableVRPOnPage">
+									<div class="onoffswitch-inner"></div>
+									<div class="onoffswitch-switch"></div>
+								</label>
+							</div>
+						</div>
+					</section>
+
+					<section class="cc-vrp-section">
+						<div class="cc-vrp-label">
+							Number of posts to display
+						</div>
+						<div class="cc-vrp-input">
+							<input type="number" style="width: 50px;" name="numberOfDisplayedPosts" id="numberOfDisplayedPosts" value="<?php echo $numberOfPosts; ?>" /> 
+						</div>
+					</section>
+					
+					<section class="cc-vrp-section">
+						<div class="cc-vrp-label">
+							Only display specific post types
+						</div>
+						<div class="cc-vrp-input">
+							<div class="onoffswitch" style="clear: both;">
+								<input style="display: none;" type="checkbox" name="customPostTypesToUse" class="onoffswitch-checkbox" id="customPostTypesToUse" <?php if ($checked == "on") echo "checked" ?>>
+								<label class="onoffswitch-label" for="customPostTypesToUse">
+									<div class="onoffswitch-inner"></div>
+									<div class="onoffswitch-switch"></div>
+								</label>
+							</div>
+						</div>
+					</section>
+
+					<section class="cc-vrp-section customPostTypesToUse" <?php if ($checked == "off") echo "style='display: none;'"; ?>>
+						<div class="cc-vrp-label">
+							Post Types to use
+						</div>
+						<div class="cc-vrp-input">
+							<?php
+							foreach ($availablePostTypes as $type):
+								$typeName = get_post_type_object($type);
+								$typeName = $typeName->label;
+								?>
+								<label for="<?php echo $type ?>">
+									<input type="checkbox" name='<?php echo $type ?>' id='<?php echo $type ?>' value="<?php echo $type ?>" <?php if (array_key_exists('checkedTypes', $values) && strpos($values['checkedTypes'][0], $type)) echo 'checked'?>>
+									<?php echo $typeName ?><br>
+								</label>
+								<!-- <div class="onoffswitch" style="clear: both; float: left">
+									<input style="display: none;" type="checkbox" name="<?php echo $type ?>" class="onoffswitch-checkbox" id="<?php echo $type ?>" <?php if (array_key_exists('checkedTypes', $values) && strpos($values['checkedTypes'][0], $type)) echo 'checked'?>>
+									<label class="onoffswitch-label" for="<?php echo $type ?>">
+										<div class="onoffswitch-inner"></div>
+										<div class="onoffswitch-switch"></div>
+									</label>
+								</div>
+								<label style="display: inline-block; padding-left: 10px;" for="<?php echo $type ?>">
+									<?php echo $typeName ?>
+								</label><br> -->
+							<?php endforeach; ?>
+						</div>	
+					</section>
+				</div>
+				<?php
+			}
+
+			/**
+			 * Save Metabox data
+			 */
+			function saveVRPMetabox($post_id)
+			{
+				$availablePostTypes = get_post_types();
+
+			    // Bail if we're doing an auto save  
+			    if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return $post_id; 
+			     
+			    // if our nonce isn't there, or we can't verify it, bail 
+			    if( !isset( $_POST['meta_box_nonce'] ) || !wp_verify_nonce( $_POST['meta_box_nonce'], 'my_meta_box_nonce' ) ) return $post_id; 
+			     
+			    // if our current user can't edit this post, bail
+			    if( !current_user_can( 'edit_post' ) ) return $post_id;
+			      			      
+			    // get wherever to display the related posts or not
+			   	$display = isset($_POST['disableVRPOnPage']) ? "on" : "off";
+			   	update_post_meta($post_id, 'disableVRPOnPage', $display);
+
+			    // Get number of posts to display
+			    if (isset($_POST['numberOfDisplayedPosts'])) update_post_meta($post_id, 'numberOfDisplayedPosts', wp_kses( $_POST['numberOfDisplayedPosts']));
+			    	else update_post_meta($post_id, 'numberOfDisplayedPosts', $this->cc_vrp_options['defaultNumberOfPosts']);
+			
+				// verify if checked to use custom post types for current article
+			    $checked = isset($_POST['customPostTypesToUse']) ? "on" : "off";
+			    update_post_meta($post_id, 'customPostTypesToUse', $checked);
+
+			    // if is checked... save all post types
+			    if ($checked == 'on'):
+			    	$checkedPostTypes = array();
+			    	foreach($availablePostTypes as $type):
+						if (isset($_POST[$type]))
+							$checkedPostTypes[] = $type;
+					endforeach;
+					update_post_meta($post_id, 'checkedTypes', $checkedPostTypes);
+			    endif;
+			}
+		}
+	endif;
+	
+?>
